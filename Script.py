af881b1d0b3c6513f73c270db6ecb2d823bff6a4
Hemant Kumar <160343394+Speed10x@users.noreply.github.com>
2025-02-15 13:25:01 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -65,7 +65,7 @@ BOT WORKING PROPERLY 🔥"""
 📅 Dᴀᴛᴇ : <code>{}</code>
 ⏰ Tɪᴍᴇ : <code>{}</code>
 🌐 Tɪᴍᴇᴢᴏɴᴇ : <code>Asia/Kolkata</code>
-🛠️ Bᴜɪʟᴅ Sᴛᴀᴛᴜs: <code>v4.2 [ Sᴛᴀʙʟᴇ ]</code>
+🛠️ Bᴜɪʟᴅ Sᴛᴀᴛᴜs: <code>v5.1 [ Sᴛᴀʙʟᴇ ]</code>
 
 Bʏ @maximize0</b>"""
         
