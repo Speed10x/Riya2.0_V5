572e55be62d7d69b54d2521ba0942f2d9fc8f8c9
Hemant Kumar <160343394+Speed10x@users.noreply.github.com>
2025-06-16 15:15:56 +0530

    Release v5.3 – Added new features and fixed bugs

--- info.py
+++ info.py
@@ -39,7 +39,7 @@ DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Riyav5:Riyav5@riyav5.w
 DATABASE_NAME = environ.get('DATABASE_NAME', "riyav5")
 
 # Files index database url
-FILES_DATABASE = environ.get('FILES_DATABASE', "mongodb+srv://riyav4:riyav4@cluster0.50dzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+FILES_DATABASE = environ.get('FILES_DATABASE', "mongodb+srv://aero:aero@cluster0.rgv6t6k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Cluster0')
 
 # Other Channel's
